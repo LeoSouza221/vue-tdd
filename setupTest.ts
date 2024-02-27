4e641fd04ab441a2ab3eac4a1f69267af1bf44c4
LeoSouza221 <leosouza221@gmail.com>
2024-02-27 11:41:27 -0300

    feat: initial setup

---
+++ setupTest.ts
@@ -0,0 +1,8 @@
+// import { i18n } from '@/locales'
+import * as matchers from '@testing-library/jest-dom/matchers'
+expect.extend(matchers)
+
+// afterEach(() => {
+//   i18n.global.locale = 'en'
+//   localStorage.clear()
+// })
